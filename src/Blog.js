27711a3f27016bfa1cc93e5ab1dbce1cc4e52993
MacBook <MacBook@MacBookdeMacBook-Pro-108.local>
2017-03-04 15:46:06 +0800

    qqq

--- src/Blog.js
+++ src/Blog.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import axios from "axios";
 
 class Blog extends React.Component {
+  componentDidMount(){
+    axios.get("https://raw.githubusercontent.com/newming/demodata/master/duopingshidai.json")
+    .then( res => console.log(res))
+  }
   render () {
     return(
       <div>
