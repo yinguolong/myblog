868ab0f0e9ee1801cbf907ef6edecd713852c3fe
MacBook <MacBook@MacBookdeMacBook-Pro-108.local>
2017-03-05 10:15:52 +0800

    asdad

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import axios from "axios";
+import marked from "marked";
+import hljs from "highlight.js";
 
+marked.setOptions({
+  highlight:function(code){
+    return hljs.highlightAuto(code).value;
+  }
+});
 class Post extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      data:""
+    }
+  }
+  componentDidMount(){
+    axios.get(`https://raw.githubusercontent.com/yinguolong/myblog/master/blogs/${this.props.params.title}.md`)
+    .then( res => this.setState({data:res.data}))
+  }
   render () {
     return(
       <div>
-        {this.props.params.title}
+        {this.state.data.length==0 ? "正在加载..." :
+        <div dangerouslySetInnerHTML = {{__html:marked(this.state.data)}}></div>}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Routers from "./routes";
-import "./main.css"
+import "./main.css";
+import "highlight.js/styles/agate.css";
 
 
 render(<Routers />,document.getElementById('root'));
