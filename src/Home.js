72c338d04868323fe18009e00d21b41a2ab3eb83
MacBook <MacBook@MacBookdeMacBook-Pro-108.local>
2017-03-05 15:17:57 +0800

    asa

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,11 @@ class Home extends React.Component {
     return(
       <div className = "home">
           <div className = "home-cover">
-            <h1>I LOVE YOU , [关楠]</h1>
-            <p>FrontEnd Web Developer</p>
-            <a href = "https://github.com/yinguolong">我的主页</a>
+            <div>
+              <h1>I LOVE YOU , [关楠]</h1>
+              <p>FrontEnd Web Developer</p>
+              <a href = "https://github.com/yinguolong">我的主页</a>
+            </div>
           </div>
       </div>
     )
