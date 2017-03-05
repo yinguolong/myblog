c1c7884f8788dda09288c2ff5da21384764f9ec8
MacBook <MacBook@MacBookdeMacBook-Pro-108.local>
2017-03-05 14:34:27 +0800

    qqq

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Routers from "./routes";
-import "./main.css";
-import "highlight.js/styles/agate.css";
-import "./post.css"
-
-
-render(<Routers />,document.getElementById('root'));
